c129d38ef0fa1b937b9c7181a36746eaed1bf042
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-01-22 16:44:13 -0300

    feat: criação da arquitetura incial

---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://9t2mw7tno9.execute-api.us-east-1.amazonaws.com/api/auth'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require('./package.json').dependencies;
+
+const shared = {
+  "@angular/core": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/core"] },
+  "@angular/common": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/common"] },
+  "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/common"] },
+  "@angular/router": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/router"] },
+  "@angular/forms": { singleton: true, strictVersion: true, requiredVersion: deps["@angular/forms"] },
+  "rxjs": { singleton: true, strictVersion: true, requiredVersion: deps["rxjs"] }
+};
 
 module.exports = {
-  output: { uniqueName: "vyracare_app_user_mfe", publicPath: "auto" },
+  output: {
+    uniqueName: "vyracare_app_user_mfe",
+    publicPath: "auto",
+    scriptType: 'module'
+  },
   optimization: { runtimeChunk: false },
+  experiments: {
+    outputModule: true
+  },
   plugins: [
     new ModuleFederationPlugin({
-      name: "vyracare_app_user_mfe",
+      name: "user",
       filename: "remoteEntry.js",
+      library: { type: 'module' },
       exposes: {
-        './App': './src/app/app.ts'
+        './App': './src/app/app.ts',
+        './Routes': './src/app/app.routes.ts'
       },
-      shared: ["@angular/core", "@angular/common", "@angular/router"]
+      shared
     })
   ],
 };
