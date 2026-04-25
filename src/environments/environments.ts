1720c47780e47e86cb76fa1eed1e63647e884ccd
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-04-25 11:35:19 -0300

    fix: atualiza url da api client no app user mfe

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   authApiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/auth',
-  clientApiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/client'
+  clientApiUrl: 'https://3j2875d4vl.execute-api.us-east-1.amazonaws.com/api/client'
 };
