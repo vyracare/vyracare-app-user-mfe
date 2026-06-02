fa0bda6667dc933b51f135927ac03a440dab79bc
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-06-02 09:59:25 -0300

    fix: corrige environment invalido

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,5 @@
-export const environment = {
+﻿export const environment = {
   production: false,
-  clientApiUrl: 'https://4uh1kerr9a.execute-api.us-east-1.amazonaws.com/api/client',,
   authApiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/auth',
   clientApiUrl: 'https://4uh1kerr9a.execute-api.us-east-1.amazonaws.com/api/client'
 };
